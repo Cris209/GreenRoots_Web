53f36c69e4042df8da306e2af7b0cd4a686388c2
Cris209 <162402416+Cris209@users.noreply.github.com>
2025-09-23 22:52:59 -0600

    Update_script_v1.js

--- frontend/script.js
+++ frontend/script.js
@@ -5,7 +5,7 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
   const password = document.getElementById("password").value;
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/api/login", {
+    const response = await fetch("https://greenroots-web.onrender.com/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ correo, password })
